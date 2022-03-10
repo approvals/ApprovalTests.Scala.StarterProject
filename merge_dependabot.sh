dcc3c5a1fbc8381dff05d1efa46d06babdc16bf7
Scott Wierschem <scottw@potentialpower.com>
2022-03-10 16:58:25 -0600

    e Remove guard clause

--- merge_dependabot.sh
+++ merge_dependabot.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-if [[ -z "${GITHUB_TOKEN}" ]]; then
 gh pr merge --auto --rebase "$PR_URL"
-else
-echo 'if you want github to automatically merge pull requests from dependdabot, you need to create a' 
-echo 'personal access token (public_repo) and assign it to setings -> secrets -> dependabot -> GH_ACTION_TOKEN'
-fi
