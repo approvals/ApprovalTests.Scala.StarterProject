3d39597cc409b56e8908b31155a5d48ddd39c8b7
Scott Wierschem <scottw@potentialpower.com>
2022-03-10 16:07:27 -0600

    F Use normal JUnit

--- app/src/test/scala/ApprovalTests/Scala/StarterProject/AppSuite.scala
+++ app/src/test/scala/ApprovalTests/Scala/StarterProject/AppSuite.scala
@@ -1,9 +1,12 @@
 package ApprovalTests.Scala.StarterProject
 
 import org.approvaltests.Approvals
-import org.junit.Test
+import org.junit.{Assert, Test}
 
 class AppSuiteTest {
+  @Test def testNormalJUnitTest(): Unit = {
+    Assert.assertEquals(5, 5)
+  }
   @Test def testUsingApprovalTests(): Unit = {
     Approvals.verify("Hello World")
   }
