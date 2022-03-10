42d5e82d1b734ce5a3fdcdbbbf131bdd60f0d060
Scott Wierschem <scottw@potentialpower.com>
2022-03-10 16:24:42 -0600

    F Add Approvals and JSON

--- app/src/test/scala/ApprovalTests/Scala/StarterProject/AppSuite.scala
+++ app/src/test/scala/ApprovalTests/Scala/StarterProject/AppSuite.scala
@@ -1,6 +1,6 @@
 package ApprovalTests.Scala.StarterProject
 
-import org.approvaltests.Approvals
+import org.approvaltests.{Approvals, JsonApprovals}
 import org.approvaltests.combinations.CombinationApprovals
 import org.junit.{Assert, Test}
 
@@ -13,5 +13,15 @@ class AppSuiteTest {
   }
   @Test def testUsingApprovalCombinations(): Unit = {
     val numbers: Array[Integer] = Array( 10 , 20 , 30 , 40 , 50 )
-    CombinationApprovals.verifyAllCombinations( (a: Integer , b: Integer) => s"$a + $b" , numbers, numbers)}
+    CombinationApprovals.verifyAllCombinations( (a: Integer , b: Integer) => s"$a + $b" , numbers, numbers)
+  }
+
+  /**
+   * note: this requires GSON which is currently added in the build.gradle file.
+   * This is only required if you want to use the VerifyAsJson.
+   */
+  @Test def testUsingApprovalsAndJson () {
+    def hero = new Person("jayne", "cobb", true, 38)
+    JsonApprovals.verifyAsJson(hero)
+  }
 }
---
+++ app/src/test/scala/ApprovalTests/Scala/StarterProject/Person.scala
@@ -0,0 +1,5 @@
+package ApprovalTests.Scala.StarterProject
+
+case class Person(firstName: String, lastName: String, isMale: Boolean, age: Int) {
+
+}
